c4599d12386cc32e8740a2595aec537e9f3507bb
lorofer <Lev.buben@gmail.com>
2025-03-24 13:15:37 +0300

    исправлен еще один баг

--- frontend/src/components/ConfirmEmailForm/ConfirmEmailForm.jsx
+++ frontend/src/components/ConfirmEmailForm/ConfirmEmailForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import './ConfirmEmailForm.scss'
-import {useNavigate} from "react-router-dom";
 
 export default function ConfirmEmailForm() {
     const navigate = useNavigate();
--- frontend/src/components/SignUpForm/SignUpForm.jsx
+++ frontend/src/components/SignUpForm/SignUpForm.jsx
@@ -42,7 +42,6 @@ export default function SignUpForm() {
                 })
             })
                 .then(() => {
-                    const navigate = useNavigate();
                     navigate("confirm-email");
                 })
         } else {
